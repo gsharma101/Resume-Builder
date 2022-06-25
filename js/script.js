dd2113bff2bfa5541781c03fc89ef0db329315f9
Gaurav Sharma <56496704+gsharma101@users.noreply.github.com>
2022-06-25 21:03:08 +0530

    resumeData update

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+// ? CRUD operation variables begins from here
+// const addData = document.querySelector('.add__btn');
+// const saveData = document.querySelector('#savebtn');
+// const editData = document.querySelector('');
+// const deleteData = document.querySelector('');
+
+// ! Event Listeners begins from here
+
+// ? Add data 
+const addDataEvent = function(){
+
+};
+
+// ? Update Data
+const updateDataEvent = function(){
+
+};
+
+// ? Remove Data
+const removeDataEvent = function(){
+
+};
